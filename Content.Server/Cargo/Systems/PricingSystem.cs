5e280ec865e5e46e2a3da286f6e8edd2313976ee
ilikeships <105243738+ilikeships@users.noreply.github.com>
2024-07-27 19:52:28 +0300

    Removes an instance of 'spesos' in hardcode.

--- Content.Server/Cargo/Systems/PricingSystem.cs
+++ Content.Server/Cargo/Systems/PricingSystem.cs
@@ -79,11 +79,11 @@ public sealed class PricingSystem : EntitySystem
                     mostValuable.Pop();
             });
 
-            shell.WriteLine($"Grid {gid} appraised to {value} spesos.");
+            shell.WriteLine($"Grid {gid} appraised to {value} credits.");
             shell.WriteLine($"The top most valuable items were:");
             foreach (var (price, ent) in mostValuable)
             {
-                shell.WriteLine($"- {ToPrettyString(ent)} @ {price} spesos");
+                shell.WriteLine($"- {ToPrettyString(ent)} @ {price} credits");
             }
         }
     }
