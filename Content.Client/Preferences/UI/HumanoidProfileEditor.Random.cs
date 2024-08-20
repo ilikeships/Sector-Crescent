7d269b68ccc898dbaaa5b7ba9786b4fb51d9f5e8
dingleberrypicker <103054773+dingleberrypicker@users.noreply.github.com>
2024-08-20 22:08:10 +0200

    over 20k, fee is ten perc. under its 500

--- Content.Client/Preferences/UI/HumanoidProfileEditor.Random.cs
+++ Content.Client/Preferences/UI/HumanoidProfileEditor.Random.cs
@@ -24,7 +24,7 @@ namespace Content.Client.Preferences.UI
                     return b.Next(HumanoidCharacterProfile.DefaultBalance - 1000, HumanoidCharacterProfile.DefaultBalance + 1000);
                 }
                 int MONEY_AFTER_DEBT = Profile.BankBalance - 500; //under 0, death fee is 500
-                if (Profile.BankBalance > 0) MONEY_AFTER_DEBT = Profile.BankBalance - ((Profile.BankBalance / 100) * 10); //over 0, death fee is 10 percent. Minimum is 1 since its an int
+                if (Profile.BankBalance > 20000) MONEY_AFTER_DEBT = Profile.BankBalance - ((Profile.BankBalance / 100) * 10); //over 20k, death fee is 10 percent. Minimum is 1 since its an int
 
                 if (MONEY_AFTER_DEBT < -2000)
                 {
