47c508b3ba0ef9ca84209221d00e5e4d7c113575
Rane <elijahrane@gmail.com>
2024-09-23 12:08:40 -0400

    more test fixes

--- Content.IntegrationTests/Tests/GameRules/FailAndStartPresetTest.cs
+++ Content.IntegrationTests/Tests/GameRules/FailAndStartPresetTest.cs
@@ -54,7 +54,7 @@ public sealed class FailAndStartPresetTest
 
         Assert.That(server.CfgMan.GetCVar(CCVars.GridFill), Is.False);
         Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyFallbackEnabled), Is.True);
-        Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyDefaultPreset), Is.EqualTo("secret"));
+        Assert.That(server.CfgMan.GetCVar(CCVars.GameLobbyDefaultPreset), Is.EqualTo("adventure"));
         server.CfgMan.SetCVar(CCVars.GridFill, true);
         server.CfgMan.SetCVar(CCVars.GameLobbyFallbackEnabled, false);
         server.CfgMan.SetCVar(CCVars.GameLobbyDefaultPreset, "TestPreset");
