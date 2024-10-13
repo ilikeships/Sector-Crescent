a5c5a3505af91c28179e8da3ee873279b3fa73c1
Rane <elijahrane@gmail.com>
2024-10-13 19:40:39 -0400

    console command

--- Content.Server/_Crescent/Diplomacy/DiplomacySystem.Commands.cs
+++ Content.Server/_Crescent/Diplomacy/DiplomacySystem.Commands.cs
@@ -2,6 +2,7 @@ using Content.Shared._Crescent.Diplomacy;
 using Robust.Shared.Console;
 using Content.Server.Administration;
 using Content.Shared.Administration;
+using Content.Shared.Shuttles.Components;
 
 
 namespace Content.Server._Crescent.Diplomacy;
@@ -19,6 +20,10 @@ public partial class DiplomacySystem
         _conHost.RegisterCommand("changefactionrelations", "Changes relations between 2 factions", "changefactionrelations <faction 1 ID> <faction 2 ID> <new relation>",
             ChangeFactionRelationsCmd,
             ChangeFactionRelationsCompletion);
+
+        _conHost.RegisterCommand("changeifffaction", "Changes the faction of a UID with IFF", "changeifffaction <uid> <faction ID>",
+            ChangeIFFFactionCmd,
+            ChangeIFFFactionCompletion);
     }
 
     [AdminCommand(AdminFlags.Logs)]
@@ -93,6 +98,38 @@ public partial class DiplomacySystem
                 return CompletionResult.FromOptions(opts2);
             case 3:
                 return CompletionResult.FromHint("Ally, Neutral, ColdWar, War");
+        }
+
+        return CompletionResult.Empty;
+    }
+
+    [AdminCommand(AdminFlags.Fun)]
+    private void ChangeIFFFactionCmd(IConsoleShell shell, string argstr, string[] args)
+    {
+        if (args.Length != 2)
+        {
+            shell.WriteError("Two arguments.");
+            return;
+        }
+
+        if (!EntityUid.TryParse(args[0], out var uid))
+        {
+            shell.WriteError("Invalid UID.");
+            return;
+        }
+
+        _shuttleSystem.SetIFFFaction(uid, args[1]);
+    }
+
+    public CompletionResult ChangeIFFFactionCompletion(IConsoleShell shell, string[] args)
+    {
+        switch (args.Length)
+        {
+            case 1:
+                return CompletionResult.FromHint("A grid with an IFF component");
+            case 2:
+                var opts2 = CompletionHelper.PrototypeIDs<DiplomacyPrototype>();
+                return CompletionResult.FromOptions(opts2);
         }
 
         return CompletionResult.Empty;
