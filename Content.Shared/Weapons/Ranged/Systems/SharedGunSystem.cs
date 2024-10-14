a6777b8ab32c92db2e2d91a40a26efe71d7c1025
Rane <elijahrane@gmail.com>
2024-10-14 14:53:50 -0400

    SHIP IT

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
+using Content.Shared._Crescent.SpaceArtillery;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Actions;
 using Content.Shared.Administration.Logs;
@@ -392,7 +393,15 @@ public abstract partial class SharedGunSystem : EntitySystem
 
         var targetMapVelocity = gunVelocity + direction.Normalized() * speed;
         var currentMapVelocity = Physics.GetMapLinearVelocity(uid, physics);
-        var finalLinear = physics.LinearVelocity + targetMapVelocity - currentMapVelocity;
+        var finalLinear = physics.LinearVelocity;
+
+        // ship projectiles inherit the velocity of their grid
+        if (HasComp<ShipWeaponProjectileComponent>(uid))
+            finalLinear = physics.LinearVelocity + targetMapVelocity + currentMapVelocity;
+        // normal guns do not because tunneling is le bad when your target is a single entity
+        else
+            finalLinear = physics.LinearVelocity + targetMapVelocity - currentMapVelocity;
+
         Physics.SetLinearVelocity(uid, finalLinear, body: physics);
 
         var projectile = EnsureComp<ProjectileComponent>(uid);
