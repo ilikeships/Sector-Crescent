dcd9bede7205056d43e8b4a0f241ff6aa971fbcf
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-10-20 22:12:16 +0300

    ig nore this

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -117,6 +117,7 @@ namespace Content.Client.Entry
             _prototypeManager.RegisterIgnore("alertLevels");
             _prototypeManager.RegisterIgnore("nukeopsRole");
             _prototypeManager.RegisterIgnore("ghostRoleRaffleDecider");
+            _prototypeManager.RegisterIgnore("gcQueue");
 
             _componentFactory.GenerateNetIds();
             _adminManager.Initialize();
