42316bab5506345ea730c5b980c417ffe6f8cb73
Rane <60792108+Elijahrane@users.noreply.github.com>
2024-11-02 08:52:21 -0400

    Shields (#389)
    * Oval technology
    
    * Fit grid pretty well
    
    * It's gem
    
    * Working again sort of
    
    * deflection behavior seems okay
    
    * It's perfect
    
    * okay NOW I am happy
    
    * Almost working
    
    * wow. We did it reddit
    
    * split off ship visuals component
    
    * draw shields
    
    * good night
    
    * git rid of interior shield entity in favor of just adding more fixtures
    
    * update generation
    
    * fix shield drawing
    
    * draw below FOV but unlit
    
    * Add API and new command for removing a shield
    
    * update engine
    
    * shield emitter emits with power
    
    * Log damage taken
    
    * change load
    
    * Add better backup shield (need to address shooting inside shield later)
    
    * only deflect ship projectiles for right now
    
    * fix yaml error
    
    * Torpedo changes
    
    * Tweak numbers and emp quite a bit
    
    * fix collision
    
    * Ambient sound
    
    * point light stuff (needs a new mask)
    
    * Few more tweaks and a temp mask
    
    * sounds for powering up and powering down
    
    * Add examine
    
    * Add power switch

---
+++ Content.Server/_Crescent/ShipShields/ShipShieldsSystem.Emitter.cs
@@ -0,0 +1,100 @@
+using Content.Shared._Crescent.ShipShields;
+using Content.Server.Power.Components;
+using Content.Shared.Projectiles;
+using Robust.Shared.Physics.Components;
+using Content.Server.Emp;
+using Content.Server.Explosion.EntitySystems;
+using Content.Server.Station.Systems;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Audio;
+using Content.Shared.Examine;
+
+namespace Content.Server._Crescent.ShipShields;
+public partial class ShipShieldsSystem
+{
+    private const float MAX_EMP_DAMAGE = 10000f;
+    [Dependency] private readonly TriggerSystem _trigger = default!;
+    [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    public void InitializeEmitters()
+    {
+        SubscribeLocalEvent<ShipShieldEmitterComponent, PowerChangedEvent>(OnPowerChanged);
+        SubscribeLocalEvent<ShipShieldEmitterComponent, ShieldDeflectedEvent>(OnShieldDeflected);
+        SubscribeLocalEvent<ShipShieldEmitterComponent, ExaminedEvent>(OnExamined);
+    }
+
+    private void OnPowerChanged(EntityUid uid, ShipShieldEmitterComponent component, PowerChangedEvent args)
+    {
+        var parent = Transform(uid).GridUid;
+
+        if (parent == null)
+            return;
+
+        var filter = _station.GetInOwningStation(uid);
+
+        if (args.Powered)
+        {
+            var shield = ShieldEntity(parent.Value, source: uid);
+            if (shield != EntityUid.Invalid)
+            {
+                component.Shield = shield;
+                component.Shielded = parent.Value;
+            }
+
+            _audio.PlayGlobal(component.PowerUpSound, filter, true, component.PowerUpSound.Params);
+        }
+        else
+        {
+            UnshieldEntity(parent.Value);
+            component.Shield = null;
+            component.Shielded = null;
+
+            _audio.PlayGlobal(component.PowerDownSound, filter, true, component.PowerUpSound.Params);
+        }
+    }
+
+    private void OnShieldDeflected(EntityUid uid, ShipShieldEmitterComponent component, ShieldDeflectedEvent args)
+    {
+        if (TryComp<EmpOnTriggerComponent>(args.Deflected, out var emp))
+        {
+            component.Damage += Math.Clamp(emp.EnergyConsumption, 0f, MAX_EMP_DAMAGE);
+            _trigger.Trigger(args.Deflected);
+            QueueDel(args.Deflected);
+            return;
+        }
+
+        if (TryComp<ProjectileComponent>(args.Deflected, out var proj))
+            component.Damage += (float) proj.Damage.GetTotal();
+        else if (TryComp<PhysicsComponent>(args.Deflected, out var phys))
+            component.Damage += phys.FixturesMass;
+    }
+
+    private void OnExamined(EntityUid uid, ShipShieldEmitterComponent component, ExaminedEvent args)
+    {
+        if (!args.IsInDetailsRange)
+            return;
+
+        if (component.Damage == 0f)
+        {
+            args.PushMarkup(Loc.GetString("shield-emitter-examine-undamaged"));
+            return;
+        }
+
+        var additionalLoad = (float) Math.Pow(component.Damage, component.DamageExp);
+        var ratio = additionalLoad / component.BaseDraw;
+        ratio = (float) Math.Ceiling(ratio * 100);
+
+        args.PushMarkup(Loc.GetString("shield-emitter-examine-damaged", ("percent", ratio)));
+    }
+
+    private void AdjustEmitterLoad(EntityUid uid, ShipShieldEmitterComponent? emitter = null, ApcPowerReceiverComponent? receiver = null)
+    {
+        if (!Resolve(uid, ref emitter, ref receiver))
+            return;
+
+        /// Raise damage to the power of the growth exponent
+        var additionalLoad = (float) Math.Pow(emitter.Damage, emitter.DamageExp);
+
+        receiver.Load = emitter.BaseDraw + additionalLoad;
+    }
+}
---
+++ Content.Shared/_Crescent/ShipShields/ShipShieldEmitterComponent.cs
@@ -0,0 +1,39 @@
+using Content.Shared.DoAfter;
+using Robust.Shared.Audio;
+using Robust.Shared.Containers;
+
+namespace Content.Shared._Crescent.ShipShields;
+
+[RegisterComponent]
+public sealed partial class ShipShieldEmitterComponent : Component
+{
+    public EntityUid? Shield;
+    public EntityUid? Shielded;
+
+    [DataField]
+    public float Accumulator;
+
+    [DataField]
+    public float Damage = 0f;
+
+    [DataField]
+    public float DamageExp = 1.2f;
+
+    [DataField]
+    public float HealPerSecond = 200f;
+
+    [DataField]
+    public float UnpoweredBonus = 2f;
+
+    [DataField]
+    public float BaseDraw = 50000f;
+
+    /// <summary>
+    /// On power up, players for all on vessel, pitched down.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier PowerUpSound = new SoundPathSpecifier("/Audio/Effects/teleport_arrival.ogg");
+
+    [DataField]
+    public SoundSpecifier PowerDownSound = new SoundPathSpecifier("/Audio/Effects/teleport_departure.ogg");
+}
