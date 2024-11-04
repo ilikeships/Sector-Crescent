0cd2921c40ae757a8b78ea03b867a9fdb655af3d
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-11-04 22:45:48 +0200

    Update PassiveRegenerationSystem.cs

--- Content.Shared/_Crescent/PassiveRegeneration/PassiveRegenerationSystem.cs
+++ Content.Shared/_Crescent/PassiveRegeneration/PassiveRegenerationSystem.cs
@@ -1,5 +1,8 @@
+using Content.Shared.Damage;
+using Content.Shared.Damage.Prototypes;
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Nutrition.EntitySystems;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Crescent.PassiveRegeneration;
 
@@ -11,21 +14,37 @@ public sealed class PassiveRegenerationSystem : EntitySystem
 
     [Dependency] private readonly ThirstSystem _thirst = default!;
     [Dependency] private readonly HungerSystem _hunger = default!;
+    [Dependency] private readonly PrototypeManager _prototype = default!;
+
+    EntityQuery<PassiveRegenerationComponent> _componentQuery;
 
     private float accumulator = 0f;
-    /// <inheritdoc/>
-    public override void Initialize()
-    {
-        
-    }
+  
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
         accumulator += frameTime;
-        if (accumulator > 1f)
+        if (accumulator > 15f)
         {
+            var query = EntityQueryEnumerator<PassiveRegenerationComponent>();
+            while (query.MoveNext(out var uid, out var regenComp))
+            {
+                if(!TryComp<DamageableComponent>(uid, out var damageable))
+                    continue;
+                if(damageable.TotalDamage == 0)
+                    continue;
+                if(!TryComp<ThirstComponent>(uid, out var thirst) || !TryComp<HungerComponent>(uid, out var hunger));
+                    continue;
+                    var totalHeal = (thirst.CurrentThirst / thirst.ThirstThresholds[thirst.LastThirstThreshold]);
+                    totalHeal *= (hunger.CurrentHunger / hunger.Thresholds[hunger.CurrentThreshold]);
+                    totalHeal *= 4f;
+                    foreach(var (damageType, damageAmount) in damageable.Damage.GetDamagePerGroup(_prototype))
+                    {
+                        
+                    }
 
+            }
         }
     }
 }
