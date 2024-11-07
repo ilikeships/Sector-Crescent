1508f209d8ad2b127bb1c3923c25aeeff24f6061
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-11-08 01:32:04 +0200

    aa

--- Content.Shared/PDA/SharedPdaSystem.cs
+++ Content.Shared/PDA/SharedPdaSystem.cs
@@ -30,6 +30,7 @@ namespace Content.Shared.PDA
             ItemSlotsSystem.AddItemSlot(uid, PdaComponent.PdaPenSlotId, pda.PenSlot);
             ItemSlotsSystem.AddItemSlot(uid, PdaComponent.PdaPaiSlotId, pda.PaiSlot);
             ItemSlotsSystem.AddItemSlot(uid, PdaComponent.PdaBookSlotId, pda.BookSlot);
+            ItemSlotsSystem.AddItemSlot(uid, PdaComponent.PdaShuttleSlot, pda.shipSlot);
 
             UpdatePdaAppearance(uid, pda);
         }
