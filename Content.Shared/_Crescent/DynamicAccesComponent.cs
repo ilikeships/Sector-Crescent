7c4f5b2fb846f015e53eda2cbc8856bb46426ccf
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-11-25 15:34:09 +0200

    Update DynamicAccesComponent.cs

--- Content.Shared/_Crescent/DynamicAccesComponent.cs
+++ Content.Shared/_Crescent/DynamicAccesComponent.cs
@@ -1,10 +1,13 @@
+using Content.Shared.Access;
+using Robust.Shared.Prototypes;
+
 namespace Content.Shared._Crescent;
 
 /// <summary>
 /// This is used for...
 /// </summary>
 [RegisterComponent]
-public sealed partial class DynamicAccesComponent : Component
+public sealed partial class GridDynamicAccesComponent : Component
 {
-    
+    public List<ProtoId<AccessLevelPrototype>> dynamicAccesCodes = new();
 }
