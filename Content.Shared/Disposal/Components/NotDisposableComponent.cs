7986cb0fe1b2ac41f31310d19d24b108c033baef
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-12-01 23:57:51 +0200

    aadad

---
+++ Content.Shared/Disposal/Components/NotDisposableComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Disposal.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class NotDisposableComponent : Component
+{
+    
+}
