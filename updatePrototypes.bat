f5d466487433a37a22eb473d4d5a661b10614cae
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2024-12-05 22:14:05 +0200

    Update updatePrototypes.bat

--- updatePrototypes.bat
+++ updatePrototypes.bat
@@ -1,2 +1,7 @@
-git submodule update --remote --merge --init Resources/Prototypes
+cd resources/Prototypes
+git pull https://github.com/MLGTASTICa/Hullrot-YAML-Prototypes.git main
+git merge main
+cd ..
+cd ..
+git merge master
 pause
