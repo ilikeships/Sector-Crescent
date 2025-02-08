590479dadb480a96aed107b78f9ee33074662c2c
MLGTASTICa <ak9bc10d@yahoo.com>
2025-02-08 15:31:10 +0200

    Shipmodding 9/11

---
+++ Content.Shared/_Crescent/ShipBalanceEnforcement/ShipSpeedByMassAdjusterComponent.cs
@@ -0,0 +1,12 @@
+namespace Content.Shared._Crescent.ShipBalanceEnforcement;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ShipSpeedByMassAdjusterComponent : Component
+{
+    public float InitialGridMass = 0f;
+
+    public float InitialGridSpeed = 0f;
+}
