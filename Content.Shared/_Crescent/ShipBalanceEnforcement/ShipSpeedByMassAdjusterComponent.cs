c87fd73fc174fdbea6f3ae5920991fb8cd228334
MLGTASTICa <ak9bc10d@yahoo.com>
2025-02-08 15:52:34 +0200

    fix shit

--- Content.Shared/_Crescent/ShipBalanceEnforcement/ShipSpeedByMassAdjusterComponent.cs
+++ Content.Shared/_Crescent/ShipBalanceEnforcement/ShipSpeedByMassAdjusterComponent.cs
@@ -6,7 +6,8 @@ namespace Content.Shared._Crescent.ShipBalanceEnforcement;
 [RegisterComponent]
 public sealed partial class ShipSpeedByMassAdjusterComponent : Component
 {
+    [ViewVariables(VVAccess.ReadWrite), DataField]
     public float InitialGridMass = 0f;
-
+    [ViewVariables(VVAccess.ReadWrite), DataField]
     public float InitialGridSpeed = 0f;
 }
