ebc8010fe51621ab39479edd046ed33fd359b92f
MLGTASTICa <ak9bc10d@yahoo.com>
2025-02-16 05:23:11 +0200

    qq
    aa

---
+++ Content.Server/_Crescent/ProjectilePhasePreventerSystem.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using Content.Shared.Projectiles;
+using Robust.Server.GameObjects;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server._Crescent;
+
+/// <summary>
+/// This handles...
+///
+/// </summary>
+[RegisterComponent]
+public sealed partial class ProjectilePhasePreventComponent : Component
+{
+
+}
+public sealed class ProjectilePhasePreventerSystem : EntitySystem
+{
+    [Dependency] private readonly PhysicsSystem _phys = default!;
+    [Dependency] private readonly TransformSystem _trans = default!;
+    /// <inheritdoc/>
+    public override void Initialize()
+
+    {
+        SubscribeLocalEvent<ProjectilePhasePreventComponent, MoveEvent>(OnMove);
+    }
+    private void OnMove(EntityUid uid, ProjectilePhasePreventComponent comp, ref MoveEvent args)
+    {
+        if (!TryComp<PhysicsComponent>(uid, out var physComp))
+            return;
+        if (!TryComp<FixturesComponent>(uid, out var fixtComp))
+            return;
+        if (!TryComp<ProjectileComponent>(uid, out var projComp))
+            return;
+        var map = _trans.GetMapId(args.OldPosition);
+        var start = _trans.ToMapCoordinates(args.OldPosition).Position;
+        var end = _trans.ToMapCoordinates(args.NewPosition).Position;
+        var angle = (end - start);
+        CollisionRay ray = new CollisionRay(start, angle, physComp.CollisionMask);
+        foreach (var obj in _phys.IntersectRay(map, ray, angle.Length(), uid, false))
+        {
+            if (obj.HitEntity == projComp.Shooter)
+                continue;
+            if (!TryComp<PhysicsComponent>(obj.HitEntity, out var targPhysComp))
+                continue;
+            if (!TryComp<FixturesComponent>(obj.HitEntity, out var targFixtComp))
+                continue;
+            var ev = new StartCollideEvent(uid, obj.HitEntity, fixtComp.Fixtures.Keys.First(),
+                targFixtComp.Fixtures.Keys.First(), fixtComp.Fixtures.Values.First(),
+                targFixtComp.Fixtures.Values.First(), physComp, targPhysComp, obj.HitPos);
+            RaiseLocalEvent(uid, ref ev);
+        }
+        
+    }
+}
