51e8c9c4aacfb7518779a43db279096203f71423
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2025-03-01 13:23:00 +0200

    zaa

--- Content.Shared/Doors/Systems/SharedDoorSystem.cs
+++ Content.Shared/Doors/Systems/SharedDoorSystem.cs
@@ -658,7 +658,6 @@ public abstract partial class SharedDoorSystem : EntitySystem
 
         var isExternal = access.AccessLists.Any(list => list.Contains("External"));
 
-        var gridId = Transform(uid).GridUid;
         if(TryComp<DynamicCodeHolderComponent>(uid, out var codeHolder) && codeHolder.codes.Count != 0)
         {
             if (!_helpers.GetPlayerIdEntity(user.Value, out var playerId))
