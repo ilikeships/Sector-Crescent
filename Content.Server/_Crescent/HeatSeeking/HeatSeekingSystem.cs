52b3e8898397426ed69594c492faf52ac0c725b7
a_person <alexanderbeaumont05@hotmail.com>
2025-03-03 21:31:09 +0000

    Update HeatSeekingSystem.cs

--- Content.Server/_Crescent/HeatSeeking/HeatSeekingSystem.cs
+++ Content.Server/_Crescent/HeatSeeking/HeatSeekingSystem.cs
@@ -58,8 +58,8 @@ public sealed class HeatSeekingSystem : EntitySystem
                 _transform.ToMapCoordinates(shipXform.Coordinates).Position
             ); // current distance from target
 
-            if (angle > _transform.GetWorldRotation(transform) + component.FOV * Math.PI / 180f
-            || angle < _transform.GetWorldRotation(transform) - component.FOV * Math.PI / 180f) // if target is out of FOV, skip it.
+            if (angle > _transform.GetWorldRotation(transform) + component.FOV / 2 * Math.PI / 180f
+            || angle < _transform.GetWorldRotation(transform) - component.FOV / 2 * Math.PI / 180f) // if target is out of FOV, skip it.
             {
                 continue;
             }
@@ -98,6 +98,17 @@ public sealed class HeatSeekingSystem : EntitySystem
                 _transform.ToMapCoordinates(xform.Coordinates).Position,
                 _transform.ToMapCoordinates(entXform.Coordinates).Position
             ); // current distance from target
+            var angle = (
+                _transform.ToMapCoordinates(entXform.Coordinates).Position -
+                _transform.ToMapCoordinates(xform.Coordinates).Position
+            ).ToWorldAngle(); // current angle towards target
+
+            if (angle > _transform.GetWorldRotation(xform) + comp.FOV * Math.PI / 180f
+            || angle < _transform.GetWorldRotation(xform) - comp.FOV * Math.PI / 180f) // if missile missed, lose lock.
+            {
+                comp.TargetEntity = null;
+                return;
+            }
 
             var targetVelocity = _transform.ToMapCoordinates(entXform.Coordinates).Position - oldPosition; // get target velocity
             float timeToImpact = distance / (oldDistance - distance); // time it will take for the missile to reach the target
