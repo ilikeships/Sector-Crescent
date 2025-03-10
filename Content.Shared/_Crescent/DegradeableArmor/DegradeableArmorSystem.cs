cbcfbb792c8c10416e33e14485f089987c40381a
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2025-03-10 18:34:32 +0200

    aa

--- Content.Shared/_Crescent/DegradeableArmor/DegradeableArmorSystem.cs
+++ Content.Shared/_Crescent/DegradeableArmor/DegradeableArmorSystem.cs
@@ -1,25 +1,76 @@
 using Content.Shared.Armor;
+using Content.Shared.Chasm;
+using Content.Shared.Chat;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.Damage.Systems;
 using Content.Shared.FixedPoint;
+using Content.Shared.Interaction;
 using Content.Shared.Inventory;
+using Content.Shared.Popups;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._Crescent.DegradeableArmor;
 
+[Serializable, NetSerializable]
+public enum ArmorRepairMaterial
+{
+    PlasteelPlate = 1 << 0,
+    NTPolymer = 1 << 1,
+    CeramicPlate = 1 << 2,
+    SteelPlate = 1 << 3,
+    DuraThread = 1 << 4,
+    PlasmaGlass = 1 << 5,
+    Plastic = 1 << 6,
+    HomelandAlloy = 1 << 7,
+    Kevlar = 1 << 8,
+    PlasteelEncasedKevlar = 1 << 9,
+    NTCeramic = 1 << 10
+
+}
 /// <summary>
 /// This handles...
 /// </summary>
 public sealed class DegradeableArmorSystem : EntitySystem
 {
     [Dependency] private readonly StaminaSystem _stamina = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
     /// <inheritdoc/>
     public override void Initialize()
     {
         SubscribeLocalEvent<DegradeableArmorComponent, MapInitEvent>(OnInit);
         SubscribeLocalEvent<DegradeableArmorComponent, InventoryRelayedEvent<DamageModifyEvent>>(OnDamageModify);
+        SubscribeLocalEvent<ArmorRepairKitComponent, AfterInteractEvent>(OnArmorKitUse);
     }
 
+    private void OnArmorKitUse(EntityUid uid, ArmorRepairKitComponent component, ref AfterInteractEvent args)
+    {
+        if (!TryComp<DegradeableArmorComponent>(args.Target, out var targetComponent))
+            return;
+        if (targetComponent.armorRepair != component.materialType)
+        {
+            _popup.PopupClient("You can't use this material to repair this!", args.User, PopupType.Medium);
+            return;
+        }
+
+        if (targetComponent.armorHealth >= targetComponent.armorMaxHealth)
+        {
+            _popup.PopupClient("This is already in pristine condition!", args.User, PopupType.Medium);
+            return;
+        }
+
+        var usedArmor = Math.Min(component.repairHealth, targetComponent.armorMaxHealth - targetComponent.armorHealth);
+        component.repairHealth -= usedArmor;
+        if (component.repairHealth <= 0)
+        {
+            QueueDel(uid);
+        }
+
+        targetComponent.armorHealth += usedArmor;
+        _popup.PopupClient($"You use the armor kit. The armor on the target is now at {(int)targetComponent.armorHealth/targetComponent.armorMaxHealth}% health", args.User, PopupType.Medium);
+
+
+    }
     private void OnInit(EntityUid uid, DegradeableArmorComponent component, ref MapInitEvent args)
     {
         if(component.armorHealth == 0)
