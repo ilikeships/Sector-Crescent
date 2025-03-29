af9e8bf15bd28b20266deb43b8f2538e2ce618f7
a_person <alexanderbeaumont05@hotmail.com>
2025-03-29 19:58:00 +0000

    is that a jjk refrence?

--- Content.Shared/_Crescent/Magic/LesserHackSpellEvent.cs
+++ Content.Shared/_Crescent/Magic/LesserHackSpellEvent.cs
@@ -4,28 +4,19 @@ using Content.Shared.Magic;
 
 namespace Content.Shared._Crescent.Magic;
 
-public sealed partial class KnockdownSpellEvent : WorldTargetActionEvent, ISpeakSpell
+public sealed partial class LesserHackSpellEvent : EntityTargetActionEvent, ISpeakSpell
 {
     /// <summary>
     /// Sound effect for the spell.
     /// </summary>
     [DataField("sound")]
-    public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/Magic/fireball.ogg");
+    public SoundSpecifier Sound = new SoundPathSpecifier("/Audio/Magic/knock.ogg");
 
     /// <summary>
     /// Volume control for the spell.
     /// </summary>
     [DataField("volume")]
     public float Volume = 5f;
-
-    [DataField("range")]
-    public float Range = 2f;
-
-    [DataField("knockdownForce")]
-    public float KnockdownForce = 200f;
-
-    [DataField("staminaDamage")]
-    public float StaminaDamage = 90f;
 
     [DataField("speech")]
     public string? Speech { get; private set; }
